4207feb7a44b5fc329328efdf0c624ac198950c6
Marcos Suarez <negrosuarez22@gmail.com>
2021-04-07 16:15:25 -0300

    subiendo las funciones de orden para ordenar datos

--- Actividad 1/src/funciones/funciones_orden.py
+++ Actividad 1/src/funciones/funciones_orden.py
@@ -1,6 +1,30 @@
 from . import utilidades, MENSAJE_PARA_CONTINUAR
 
+from datos.datos import lista_alumnos
 
+# nombre, 1 o 2, notas_final
 def imprimir_datos_ordenados(criterio):
     """Imprime en consola los datos ordenados."""
+    lista_ordenada = ordenar(criterio)
+    
+    for alumno in lista_ordenada:
+        print('=' * 30)
+        print()
+        print(f'{alumno["nombre"]}')
+        print('Notas: ', end='')
+        for nota in alumno['notas']:
+            print(nota, end=' ')
+        print() 
+        print(f'Nota final: {alumno["nota_final"]}')
+        print()
+        print('=' * 30)
+    
     utilidades.enter_to_continue(MENSAJE_PARA_CONTINUAR)
+    
+       
+def ordenar(criterio):
+    if isinstance(criterio, int):
+        return sorted(lista_alumnos, key=lambda dicc: dicc['notas'][criterio - 1])
+    else:
+        return sorted(lista_alumnos, key= lambda dicc: dicc[criterio])    
+    
