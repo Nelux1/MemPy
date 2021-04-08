2254b83ce362ee6994d289075002f01279072e2f
Priscila Faliani <priscilafaliani@gmail.com>
2021-04-08 16:36:45 -0300

    Agrego funciones para realizar un reporte.

--- Actividad 1/src/funciones/funciones_reporte.py
+++ Actividad 1/src/funciones/funciones_reporte.py
@@ -1,11 +1,40 @@
-from . import utilidades, MENSAJE_PARA_CONTINUAR
+def iniciar_configuracion(tipo):
+    # abre el menú de configuración enviándole el tipo de reporte
+    from menus.menu_configurar_reporte import crear_menu_configurar_reporte
+    crear_menu_configurar_reporte(tipo).show()
 
 
-def imprimir_reporte(tipo):
+#  mensaje que se muestra si el rango para hacer el reporte no es válido.
+TEXTO_RANGO_INVALIDO = f'\N{cross mark} Por favor, especifique un rango numérico válido. FIN >= INICIO\n'
+
+
+def imprimir_reporte(tipo, rango):
     """Imprime en consola los resultados del reporte."""
+    from . import utilidades, MENSAJE_PARA_CONTINUAR
+    
+    inicio, fin = rango
+    
+    if fin < inicio:
+        utilidades.println(TEXTO_RANGO_INVALIDO)
+    else:
+        utilidades.println(f'Reporte sobre {tipo_str(tipo)}, en el rango ({inicio}, {fin})\n')
+        # [utilidades.println(f'{nombre}\n\n{"=" * 30}') for nombre in realizar_reporte(tipo, inicio, fin)]
     utilidades.enter_to_continue(MENSAJE_PARA_CONTINUAR)
 
 
-# pseudo constantes -> textos para la pantalla al obtener rango del reporte.
-TEXTO_RANGO_INDICACIONES = f'Indique el rango de notas sobre el cual realizar el reporte \N{maple leaf}\n\nSi ambos valores del rango son iguales, sólo se incluirán las personas con ese valor específico!\n'
-TEXTO_RANGO_INVALIDO = f'\N{cross mark} Por favor, especifique un rango numérico válido. INICIO >= FIN y ambos deben ser >= 0\n'
+def realizar_reporte(tipo, inicio, fin):
+    """Retorna lista de los alumnos que entren en el rango (inicio, fin) según tipo."""
+    from datos.datos import lista_alumnos
+    
+    if tipo == 'nota_final':
+        return [alumno['nombre'] for alumno in lista_alumnos if alumno[tipo] >= inicio and alumno[tipo] <= fin]
+
+    return [alumno['nombre'] for alumno in lista_alumnos if alumno['notas'][tipo] >= inicio and alumno['notas'][tipo] <= fin]
+
+
+def tipo_str(tipo):
+    """Retorna una versión legible del tipo de reporte."""
+    if tipo == 'nota_final':
+        return 'nota final'
+    
+    return f'evaluación {tipo}'
