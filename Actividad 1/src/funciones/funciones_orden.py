9fa932e671ce6f06d97e5af738c50e19c8accc8d
Priscila Elizabeth Faliani <priscilafaliani@gmail.com>
2021-04-13 01:22:48 -0300

    Cree un método para ordenar por clave, otro para evaluación y otro para imprimir un alumno.

--- Actividad 1/src/funciones/funciones_orden.py
+++ Actividad 1/src/funciones/funciones_orden.py
@@ -2,36 +2,34 @@ from . import utilidades, MENSAJE_PARA_CONTINUAR
 
 from datos.datos import lista_alumnos
 
-# nombre, 1 o 2, notas_final
-def imprimir_datos_ordenados(criterio):
-    """Imprime en consola los datos ordenados."""
-    lista_ordenada = ordenar(criterio)
-    
-    for alumno in lista_ordenada:
-        print('=' * 30)
-        print()
-        print(f'{alumno["nombre"]}')
-        print('Notas: ', end='')
-        for nota in alumno['notas']:
-            print(nota, end=' ')
-        print() 
-        print(f'Nota final: {alumno["nota_final"]}')
-        print()
-        print('=' * 30)
+
+def ordenar_por_clave(clave):
+    """Retorna lista de alumnos ordenadas por la clave dada."""
+    return sorted(lista_alumnos, key= lambda dicc: dicc[clave])   
+
+
+def ordenar_por_evaluacion(numero):
+    """Retorna lista de alumnos ordenadas por el número de evaluación dado."""
+    return sorted(lista_alumnos, key=lambda dicc: dicc['notas'][numero - 1])
+
+
+def imprimir_alumno(alumno):
+    """Imprime los datos de un alumno con formato."""
+    print('=' * 30)
+    print()
+    print(f'{alumno["nombre"]}')
+    print('Notas: ', end='')
+    for nota in alumno['notas']:
+        print(nota, end=' ')
+    print() 
+    print(f'Nota final: {alumno["nota_final"]}')
+    print()
+    print('=' * 30)
+
+
+def imprimir_alumnos(lista):
+    """Imprime en consola la lista de alumnos dada."""   
+    for alumno in lista:
+        imprimir_alumno(alumno)
     
     utilidades.enter_to_continue(MENSAJE_PARA_CONTINUAR)
-    
-       
-def ordenar(criterio):
-    """Retorna la lista de alumnos ordenada segun el criterio dado.txt
-    
-        Si recibe un int, se supone que se desea ordenar en base a
-        los resultados de una evaluación.
-        
-        Si no, puede ser por nombre o por nota final.
-    """
-    if isinstance(criterio, int):
-        return sorted(lista_alumnos, key=lambda dicc: dicc['notas'][criterio - 1])
-    else:
-        return sorted(lista_alumnos, key= lambda dicc: dicc[criterio])    
-    
