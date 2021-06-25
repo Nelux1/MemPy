191223fe77b2354e8388a655cd0952c282427dfb
ElDylon <71729737+ElDylon@users.noreply.github.com>
2021-06-25 17:20:49 -0300

    Arreglando parametros en criterios para que reciban listas y no de errores

--- MemPy/src/criterios/datasets/pokemon.py
+++ MemPy/src/criterios/datasets/pokemon.py
@@ -6,36 +6,36 @@ import csv
 pokemons_path = os.path.join('resources', 'datasets', 'pokemon', 'pokemon.csv')
 
 
-def get_pokemons_legendarios(legendario):
-    """Retorna lista de los nombres de los pokemons legendarios."""
+def get_pokemons_legendarios(legendario,poder):
+    """Retorna lista de los nombres de los pokemons legendarios con velocidad mayor a la estipulada."""
     with open(pokemons_path, 'r', encoding='utf-8') as f:
         reader = csv.reader(f)
         next(reader)
-        return [pokemon[-11] for pokemon in reader if pokemon[-1] == legendario]
+        return [pokemon[-11] for pokemon in reader if pokemon[-1] == legendario and pokemon[-6] >= poder]
         
 
-def get_pokemons_nolegendarios_detipo(tipo):
+def get_pokemons_nolegendarios_detipo(legendario,tipo):
     """Retorna lista de los nombres de pokemons tipo agua, no legendarios"""
     with open(pokemons_path, 'r', encoding='utf-8') as f:
         reader = csv.reader(f)
         next(reader)
-        return [pokemon[-11] for pokemon in reader if pokemon[-1] == '0' and pokemon[-5] == tipo]
+        return [pokemon[-11] for pokemon in reader if pokemon[-1] == legendario and pokemon[-5] == tipo]
 
 """Variable que contiene todos los criterios posibles sobre pokemons"""
 criterios = {
     'criterio1': {
         'criterio': 'Retorna lista de los nombres de los pokemons legendarios', 
         'funcion': get_pokemons_legendarios, 
-        'parametros': ('1')
+        'parametros': ('1','2')
     },
     'criterio2': {
         'criterio': 'Retorna lista de los nombres de pokemons tipo agua, no legendarios',
         'funcion': get_pokemons_nolegendarios_detipo, 
-        'parametros': ('water')
+        'parametros': ('0','water')
     },
     'criterio3': {
         'criterio': 'Retorna lista de los nombres de pokemons tipo planta, no legendarios',
         'funcion': get_pokemons_nolegendarios_detipo, 
-        'parametros': ('grass')
+        'parametros': ('0','grass')
     }
 }
