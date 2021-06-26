acb2a4d7e259f988b06534d891175ac3f74add8f
Marcos <negrosuarez22@gmail.com>
2021-06-26 16:48:34 -0300

    mensaje ganaste

--- MemPy/src/components/almacenamiento.py
+++ MemPy/src/components/almacenamiento.py
@@ -6,21 +6,20 @@ import os
 
 
 archivo2=os.path.join(os.getcwd(),'posiciones.csv')
-archivo_csv2=open(archivo2,"a")
-writer2=csv.writer(archivo_csv2)
-
 archivo=os.path.join(os.getcwd(),'datos_estadisticos.csv')
-archivo_csv=open(archivo,"a")
-data=csv.reader(archivo_csv,delimiter=',')
-writer=csv.writer(archivo_csv)
 
-def guardando_data(nombre,edad,genero,tiempo,num_partida,nivel,palabras_adivinar,evento,estado,palabra,dia,hora):   
+
+def guardando_data(nombre,edad,genero,tiempo,num_partida,nivel,palabras_adivinar,evento,estado,palabra,dia,hora):
+    archivo_csv=open(archivo,"a")
+    writer=csv.writer(archivo_csv)
     usuario=[nombre,edad,genero,tiempo,num_partida,nivel,palabras_adivinar,evento,estado,palabra,dia,hora]
     writer.writerow(usuario)
     archivo_csv.close() 
     return usuario
 
 def puntos (username,puntaje):
+  archivo_csv2=open(archivo2,"a")
+  writer2=csv.writer(archivo_csv2)
   usuario=[username,puntaje]
   writer2.writerow(usuario)
   archivo_csv2.close()
